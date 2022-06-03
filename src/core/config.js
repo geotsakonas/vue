e981bd2c49ab6b206494b16e875dc36770794870
Georgios Tsakonas <94357854+geotsakonas@users.noreply.github.com>
2022-06-03 11:31:58 +0300

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -12,20 +12,7 @@ export type Config = {
   // user
   optionMergeStrategies: { [key: string]: Function };
   silent: boolean;
-  productionTip: boolean;
-  performance: boolean;
-  devtools: boolean;
-  errorHandler: ?(err: Error, vm: Component, info: string) => void;
-  warnHandler: ?(msg: string, vm: Component, trace: string) => void;
-  ignoredElements: Array<string | RegExp>;
-  keyCodes: { [key: string]: number | Array<number> };
-
-  // platform
-  isReservedTag: (x?: string) => boolean;
-  isReservedAttr: (x?: string) => boolean;
-  parsePlatformTagName: (x: string) => string;
-  isUnknownElement: (x?: string) => boolean;
-  getTagNamespace: (x?: string) => string | void;
+  productionTip: boo: string) => string | void;
   mustUseProp: (tag: string, type: ?string, name: string) => boolean;
 
   // private
